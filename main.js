f4f76be9e188f49f3bf9dba408a1b765cac9ae77
Tuncay M. Aliev <tuncaymaliev@gmail.com>
2020-12-28 22:41:03 +0300

    third commit

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ let body = selectElement('body');
 menuToggler.addEventListener('click', function () {
   body.classList.toggle('open');
 });
+
+let imgChange = selectElement("#img1");
+
+imgChange.addEventListener("mouseover", function() {
+  document.querySelector("#img1").setAttribute("src", "./images/menu-group-2.jpg" );
+});
+
+imgChange.addEventListener("mouseout", function() {
+  document.querySelector("#img1").setAttribute("src", "./images/menu-group-1.jpg" );
+});
+
+imgChange.addEventListener("click", function() {
+  window.location.href = "http://www.midashoes.com/";
+});
