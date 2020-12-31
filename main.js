13f31ff4ecf9e15f1c652c2f6074372a8e6ba652
Tuncay M. Aliev <tuncaymaliev@gmail.com>
2020-12-31 14:44:26 +0300

    eleven

--- main.js
+++ main.js
@@ -28,28 +28,39 @@ sr.reveal(".animate-left", {
   origin: "left",
   duration: 1000,
   distance: "25rem",
-  delay: 300
+  delay: 0
 });
 
 sr.reveal(".animate-right", {
   origin: "right",
   duration: 1000,
   distance: "25rem",
-  delay: 600
+  delay: 0
 });
 
 sr.reveal(".animate-top", {
   origin: "top",
   duration: 1000,
   distance: "25rem",
-  delay: 600
+  delay: 0
 });
 
 sr.reveal(".animate-bottom", {
   origin: "bottom",
   duration: 1000,
   distance: "25rem",
-  delay: 600
+  delay: 0
 });
 
+let active = document.querySelectorAll('.nav-link')
+
+active.forEach(function(a) {
+  a.addEventListener('mouseover', function() {
+    a.classList.add("active");
+  });
+
+  a.addEventListener("mouseleave", function(){
+    a.classList.remove("active");
+  });
+});
 
